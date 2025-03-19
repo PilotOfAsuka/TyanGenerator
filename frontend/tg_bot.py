2d35318b299ef3ed1ebc9720326484d9d84e7c7d
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2025-03-19 19:58:18 +0500

    Добавил свой функции, а так же проверку состояния юзера для контроля состояния и будущих дополнений таких как генерация промптов, чутка позже сделаю рефакторинг ветки и буду думать что дальше

--- frontend/tg_bot.py
+++ frontend/tg_bot.py
@@ -3,7 +3,8 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
-
+from tools.user_state_handler import user_states
+from tools.my_func import get_id_from_message
 from backend.gemineai import get_tyan_image
 
 from config import tg_api
@@ -18,36 +19,62 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=tg_api)
 dp = Dispatcher()
 
+@dp.message(Command("start_generation"))
+async def cmd_generation(message: Message):
+    # здесь будет начало генерации
+    user_states.set_user_state(state="start_generation", user_id=get_id_from_message(message))
+    await message.answer(text="Отправьте фотографию на которую хотите дорисовать тян.")
+    pass
+
 # /start
 @dp.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer(f"Привет! Я бот который добавит тянку если получится :)\n На ваше изображение.\nВозможны искажения и прочие недочеты.\nОснованно на gemini-2.0-flash-exp от Google!")
+    user_states.init_self_states(user_id=get_id_from_message(message))
+    user_states.set_user_state(state="main", user_id=get_id_from_message(message))
+    await message.answer(f"Привет! Я бот который добавит тянку если получится :)\n На ваше изображение.\nВозможны искажения и прочие недочеты.\nОснованно на gemini-2.0-flash-exp от Google!\n")
+
+# Обработка текста
+@dp.message(lambda message: message.text)
+async def handle_text(message: Message):
+    # Обработка текста по состоянию юзера
+    if user_states.get_user_state(user_id=get_id_from_message(message)) == "generation":
+        await message.answer(text="Происходит генерация, подождите...")
+    elif user_states.get_user_state(user_id=get_id_from_message(message)) == "main":
+        await message.answer(text="Используйте команду /start_generation для старта")
+        pass
 
 
 # Обработчик изображений
 @dp.message(lambda message: message.photo)
 async def handle_photo(message: Message):
-    photo = message.photo[-1]  # Берем наибольшее по размеру изображение
-    file = await bot.get_file(photo.file_id)
-    file_path = file.file_path
+    if user_states.get_user_state(user_id=get_id_from_message(message)) == "start_generation":
+        photo = message.photo[-1]  # Берем наибольшее по размеру изображение
+        file = await bot.get_file(photo.file_id)
+        file_path = file.file_path
 
-    # Загружаем файл в память
-    file_data = await bot.download_file(file_path)
-    image_bytes = file_data.read()
+        # Загружаем файл в память
+        file_data = await bot.download_file(file_path)
+        image_bytes = file_data.read()
 
-    # Кодируем в base64х
-    base64_image_from_user = base64.b64encode(image_bytes).decode("utf-8")
-    await message.answer(text="Начало генерации фото")
-    # Здесь мы получаем картинку
-    generated_image = get_tyan_image(base64_image=base64_image_from_user, text_input=eng_prompt)
+        # Кодируем в base64х
+        base64_image_from_user = base64.b64encode(image_bytes).decode("utf-8")
 
-    if generated_image:
-        # Отправляем сгенерированную фото пользователю
-        await message.answer(text="Сгенерированная фотография")
-        await message.answer_photo(photo=generated_image) # Отправка фото пользователю
-        print(f"Картинка отправлена пользователю: {message.from_user.username}")
+        await message.answer(text="Начало генерации фото")
+        user_states.set_user_state(state="generation", user_id=get_id_from_message(message))
+        # Здесь мы получаем картинку
+        generated_image = get_tyan_image(base64_image=base64_image_from_user, text_input=eng_prompt)
+
+        if generated_image:
+            # Отправляем сгенерированную фото пользователю
+            await message.answer(text="Сгенерированная фотография")
+            await message.answer_photo(photo=generated_image) # Отправка фото пользователю
+            print(f"Картинка отправлена пользователю: {message.from_user.username}")
+            user_states.set_user_state(state="main", user_id=get_id_from_message(message))
+        else:
+            await message.answer(text="Произошла ошибка, попробуйте еще раз или используйте другое фото")
+            user_states.set_user_state(state="main", user_id=get_id_from_message(message))
     else:
-        await message.answer(text="Произошла ошибка, попробуйте еще раз или используйте другое фото")
+        await message.answer(text="Используйте команду /start_generation для старта")
 
 
 # Функция старта бота
