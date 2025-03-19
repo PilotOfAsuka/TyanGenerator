8c69ad721891af1dddcdc52d8c03124a4e009c86
RuslanMakhanov <misterruslan1701@gmail.com>
2025-03-19 18:03:22 +0500

    Добавил функицю расширенного выбора, исправил frontend, зайди в дискорд у меня есть вопросики для того чтоб все это обхединить чтоб все заработало

--- frontend/tg_bot.py
+++ frontend/tg_bot.py
@@ -1,3 +1,5 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton  # Импортируем для создания кнопок
+
 import base64
 import logging
 from aiogram import Bot, Dispatcher
@@ -8,9 +10,14 @@ from backend.gemineai import get_tyan_image
 
 from config import tg_api
 
+# Функция чтоб считывать описание бота с текстового документа. Сделал чтоб легче было разбираться где описание поминять
+def load_description():
+    with open("description.txt", "r", encoding="utf-8") as file:
+        return file.read()
+    
 # Это каркас бота в телеграм
 
-rus_prompt = "Добавь девушку (внешность придумай сам) (позу и действие придумай на основе изображеня как бы ты паместил ее), стоящую рядом с существующим человеком)"
+# "Добавь девушку (внешность придумай сам) (позу и действие придумай на основе изображеня как бы ты паместил ее), стоящую рядом с существующим человеком)"
 eng_prompt = "Add a girlfriend (think up her appearance yourself) (think up her pose and action based on the image of how you would place her) standing next to an existing person)"
 
 logging.basicConfig(level=logging.INFO)
@@ -18,10 +25,30 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=tg_api)
 dp = Dispatcher()
 
-# /start
+# Создаем клавиатуру с кнопками
+def create_keyboard():
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="About", callback_data="about"),
+            InlineKeyboardButton(text="Advanced Prompt settings", callback_data="advanced_prompt")
+        ]
+    ])
+    return keyboard
+
+# Обработчик команды /start
 @dp.message(Command("start"))
 async def cmd_start(message: Message):
+    description = load_description()
+    await message.answer(description, parse_mode="Markdown")
     await message.answer(f"Привет! Я бот который добавит тянку если получится :)\n На ваше изображение.\nВозможны искажения и прочие недочеты.\nОснованно на gemini-2.0-flash-exp от Google!")
+
+# Обработчик нажатий на кнопки
+@dp.callback_query(lambda query: True)
+async def handle_button_click(query):
+    if query.data == "about":
+        await query.message.answer("Здесь будет описание бота, Я пока не приудмал. Может сам добавишь")
+    elif query.data == "advanced_prompt":
+        await query.message.answer("Здесь будут настройки промпта. Пока что это в разработке.")
 
 
 # Обработчик изображений
